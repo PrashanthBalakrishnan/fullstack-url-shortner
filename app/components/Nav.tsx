ec6b4c2d361fad15c219e32fad543a508b520f6a
Prashanth Balakrishnan <prashbala13@gmail.com>
2023-11-29 21:41:37 -0500

    added divider line

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -20,7 +20,7 @@ const Nav = () => {
           mobileNav && "top-[9%]",
         )}
       >
-        <ul className="flex w-full flex-col items-center gap-8 rounded-md p-8 text-2xl font-bold text-white md:w-auto md:flex-row md:gap-[4vw] md:text-[1rem]  md:text-[var(--neutral-100)]">
+        <ul className="flex w-full flex-col items-center gap-8 rounded-md p-10 text-2xl font-bold text-white md:w-auto md:flex-row md:gap-[4vw] md:text-[1rem]  md:text-[var(--neutral-100)]">
           <li>
             <Link className="hover:text-black" href="/">
               Features
@@ -36,13 +36,14 @@ const Nav = () => {
               Resources
             </Link>
           </li>
+          <div className="h-[.01rem] w-full bg-white/40" />
           <li className="md:ml-auto">
             <Link className="hover:text-black " href="/">
               Login
             </Link>
           </li>
           <li>
-            <Link className="btn rounded-full p-3" href="/">
+            <Link className="btn rounded-full  p-3" href="/">
               Sign Up
             </Link>
           </li>
